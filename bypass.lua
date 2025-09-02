09d4fb0c7392f7a0941df1a333dfb5dcf278eac8
vivilafrance <bigbig5661@gmail.com>
2025-09-02 23:16:44 +0700

    Update and rename bypassed.lua to bypass.lua

---
+++ bypass.lua
@@ -0,0 +1,56 @@
+
+local Hooked = {}
+local Detected, Kill
+local FoundAC = false
+
+-- Only run if bypass enabled
+if getgenv().AdonisBypassEnabled then
+    -- Run in proper thread
+    setthreadidentity(2)
+
+    -- Quick scan through GC to detect Adonis
+    for i, v in getgc(true) do
+        if typeof(v) == "table" then
+            local DetectFunc = rawget(v, "Detected")
+            local KillFunc = rawget(v, "Kill")
+
+            if typeof(DetectFunc) == "function" and not Detected then
+                Detected = DetectFunc
+                FoundAC = true
+                print("[Adonis Bypass] AntiCheat Detected! Bypass activated.")
+
+                local Old
+                Old = hookfunction(Detected, function(Action, Info, NoCrash)
+                    return true
+                end)
+                table.insert(Hooked, Detected)
+            end
+
+            if rawget(v, "Variables") and rawget(v, "Process") and typeof(KillFunc) == "function" and not Kill then
+                Kill = KillFunc
+                local Old
+                Old = hookfunction(Kill, function(Info)
+                    -- prevent kill
+                end)
+                table.insert(Hooked, Kill)
+            end
+        end
+    end
+
+    -- Hook debug.info to prevent crash checks
+    if Detected then
+        local Old
+        Old = hookfunction(getrenv().debug.info, newcclosure(function(...)
+            local LevelOrFunc = ...
+            if LevelOrFunc == Detected then
+                return coroutine.yield(coroutine.running())
+            end
+            return Old(...)
+        end))
+    else
+        print("[Adonis Bypass] No AntiCheat detected. Nothing to bypass.")
+    end
+
+    -- Restore thread identity
+    setthreadidentity(7)
+end
