67b8ab3cfedff8c8c34701713499af58229eeb7a
vivilafrance <bigbig5661@gmail.com>
2025-09-02 23:11:54 +0700

    Update anti.lua

--- anti.lua
+++ anti.lua
@@ -6,53 +6,56 @@ local Detected, Kill
 
 setthreadidentity(2)
 
-for i, v in getgc(true) do
-    if typeof(v) == "table" then
-        local DetectFunc = rawget(v, "Detected")
-        local KillFunc = rawget(v, "Kill")
-    
-        if typeof(DetectFunc) == "function" and not Detected then
-            Detected = DetectFunc
-            
-            local Old; Old = hookfunction(Detected, function(Action, Info, NoCrash)
-                if Action ~= "_" then
-                    if DEBUG then
-                        warn(`Adonis AntiCheat flagged\nMethod: {Action}\nInfo: {Info}`)
-                    end
-                end
+if getgenv().AdonisBypassEnabled then
+    for i, v in getgc(true) do
+        if typeof(v) == "table" then
+            local DetectFunc = rawget(v, "Detected")
+            local KillFunc = rawget(v, "Kill")
+        
+            if typeof(DetectFunc) == "function" and not Detected then
+                Detected = DetectFunc
                 
-                return true
-            end)
+                local Old; Old = hookfunction(Detected, function(Action, Info, NoCrash)
+                    if Action ~= "_" then
+                        if DEBUG then
+                            warn(`Adonis AntiCheat flagged\nMethod: {Action}\nInfo: {Info}`)
+                        end
+                    end
+                    
+                    return true
+                end)
 
-            table.insert(Hooked, Detected)
-        end
+                table.insert(Hooked, Detected)
+            end
 
-        if rawget(v, "Variables") and rawget(v, "Process") and typeof(KillFunc) == "function" and not Kill then
-            Kill = KillFunc
-            local Old; Old = hookfunction(Kill, function(Info)
-                if DEBUG then
-                    warn(`Adonis AntiCheat tried to kill (fallback): {Info}`)
-                end
-            end)
+            if rawget(v, "Variables") and rawget(v, "Process") and typeof(KillFunc) == "function" and not Kill then
+                Kill = KillFunc
+                local Old; Old = hookfunction(Kill, function(Info)
+                    if DEBUG then
+                        warn(`Adonis AntiCheat tried to kill (fallback): {Info}`)
+                    end
+                end)
 
-            table.insert(Hooked, Kill)
+                table.insert(Hooked, Kill)
+            end
         end
     end
+
+    local Old; Old = hookfunction(getrenv().debug.info, newcclosure(function(...)
+
+        local LevelOrFunc, Info = ...
+
+        if Detected and LevelOrFunc == Detected then
+            if DEBUG then
+                warn(`zins | adonis bypassed`)
+            end
+
+            return coroutine.yield(coroutine.running())
+        end
+        
+        return Old(...)
+    end))
+
+    -- setthreadidentity(9)
+    setthreadidentity(7)
 end
-
-local Old; Old = hookfunction(getrenv().debug.info, newcclosure(function(...)
-    local LevelOrFunc, Info = ...
-
-    if Detected and LevelOrFunc == Detected then
-        if DEBUG then
-            warn(`zins | adonis bypassed`)
-        end
-
-        return coroutine.yield(coroutine.running())
-    end
-    
-    return Old(...)
-end))
-
--- setthreadidentity(9)
-setthreadidentity(7)
